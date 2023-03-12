03acdb92792cbfd398c59b8caa56c7ca5cb9be58
Venkat <tech.venkat91@gmail.com>
2023-03-12 22:54:30 +0530

    first microservices as part of apigateway example

---
+++ src/main/java/com/firstmicroservices/FirstMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package com.firstmicroservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstMicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstMicroservicesApplication.class, args);
+	}
+
+}
